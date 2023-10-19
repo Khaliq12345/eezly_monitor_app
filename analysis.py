5898d43a7ce18fc4d7d73b4113dce3c970091566
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-10-19 02:58:42 +0000

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -11,7 +11,6 @@ head_tail = st.selectbox(
 )
 run_button = st.button('Run')
 
-st.secrets['']
 AWS_ACCESS_KEY_ID = st.secrets['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = st.secrets['AWS_SECRET_ACCESS_KEY']
 BUCKET = st.secrets['BUCKET']
